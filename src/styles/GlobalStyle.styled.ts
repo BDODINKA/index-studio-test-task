137de14a82e26d0a8729490840cfcb0c133ebd0b
DMITRY <bdodinka1992@gmail.com>
2023-05-17 17:13:48 +0300

    feat:add GlobalStyle

--- src/styles/GlobalStyle.styled.ts
+++ src/styles/GlobalStyle.styled.ts
@@ -1,14 +1,28 @@
-body {
-    background: black;
+import { createGlobalStyle } from "styled-components";
+import { normalize } from "styled-normalize";
+
+export const GlobalStyle = createGlobalStyle`
+  ${normalize}
+  * {
+    padding: 0;
     margin: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    background: black;
     font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
     'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
     sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
-}
+  }
 
-code {
+  code {
     font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New',
     monospace;
-}
+  }
+
+
+
+`;
